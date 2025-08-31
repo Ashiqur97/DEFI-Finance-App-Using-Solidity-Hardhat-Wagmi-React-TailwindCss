2c7a93d7f68925c52ea4ca01360ce1b212ced63d
Ashiqur97 <ashiqurrahmanabir96@gmail.com>
2025-08-31 16:00:44 +0600

    deploy file updating

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -15,5 +15,33 @@ async function main() {
     await borrowToken.deployed();
     console.log("Borrow Token deployed to", borrowToken.address);
 
+    const PriceOracle = await ethers.getContractFactory("PriceOracle");
+    const priceOracle = await PriceOracle.deploy();
+    await priceOracle.deployed();
+    console.log("Price Oracle deployed to:",priceOracle.address);
+    
+    
+    await priceOracle.setPrice(collateralToken.address, ethers.utils.parseUnits("1", 18));
+    await priceOracle.setPrice(borrowToken.address, ethers.utils.parseUnits("1", 18));
+
+    const Lending = await ethers.getContractFactory("Lending");
+    const lending = await Lending.deploy(
+        collateralToken.address,
+        borrowToken.address,
+        priceOracle.address
+    );
+    await lending.deployed();
+    console.log("Lending deployed to:",lending.address);
+
+    const Swap = await ethers.getContractFactory("Swap");
+    const swap = await Swap.deploy(
+        collateralToken.address,
+        borrowToken.address,
+        priceOracle.address
+    );
+
+    await swap.deployed();
+    console.log("Swap deployed to:", swap.address);
+
     
 }
