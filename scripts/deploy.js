8e20dedb5d3d54e4e26d2df599aeacf11d174829
Ashiqur97 <ashiqurrahmanabir96@gmail.com>
2025-08-31 15:44:46 +0600

    hardhat deploy edit

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,5 +1,19 @@
 const {ethers} = require("hardhat");
 
 async function main() {
+    const [deployer] = await ethers.getSigners();
+
+    console.log("Deploying contracts with the account", deployer.address);
+
+    const Token = await ethers.getContractFactory("Token");
+    
+    const collateralToken = await Token.deploy("Collateral Token","CT");
+
+    await collateralToken.deployed();
+
+    const borrowToken = await Token.deploy("Borrow Token","BT");
+    await borrowToken.deployed();
+    console.log("Borrow Token deployed to", borrowToken.address);
+
     
 }
