11f6b8145a3c37d927e9706868c0394ed750abb8
Ashiqur97 <ashiqurrahmanabir96@gmail.com>
2025-09-06 16:31:48 +0600

    test file updating

--- test/PriceOracle.test.js
+++ test/PriceOracle.test.js
@@ -14,6 +14,19 @@ describe("PriceOracle",function() {
 
         PriceOracle = await ethers.getContractFactory("PriceOracle");
         priceOracle = await PriceOracle.deploy();
-    })
+        await priceOracle.deployed();
+
+        const Token = await ethers.getContractFactory("Token");
+        token = await Token.deploy("Test Token","TT");
+        await token.deployed();
+    });
+
+
+  describe("Price Management", function () {
+    it("Should allow owner to set price", async function () {
+      await priceOracle.setPrice(token.address, ethers.utils.parseUnits("1", 18));
+      
+      expect(await priceOracle.getPrice(token.address)).to.equal(ethers.utils.parseUnits("1", 18));
+    });
 
 })
